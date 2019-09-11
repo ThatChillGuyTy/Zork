119189a283d9663b713f45b0582ede249e242198
ThatChillGuyTy <37196949+ThatChillGuyTy@users.noreply.github.com>
2019-09-11 15:23:05 -0400

    4

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -7,6 +7,45 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            string inputString = System.Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper();
+            Console.WriteLine(command)
         }
+
+        private static Commands ToCommand(string commandString)
+        {
+            Commands command;
+
+            if (commandString == "QUIT")
+            {
+                command = Commands.QUIT;
+            }
+            else if (commandString == "LOOK")
+            {
+                command = Commands.LOOK;
+            }
+            else if (commandString == "NORTH")
+            {
+                command = Commands.NORTH;
+            }
+            else if (commandString == "SOUTH")
+            {
+                command = Commands.SOUTH;
+            }
+            else if (commandString == "EAST")
+            {
+                command = Commands.EAST;
+            }
+            else if (commandString == "WEST")
+            {
+                command = Commands.WEST;
+            }
+            else
+            {
+                command = Commands.UNKNOWN;
+            }
+            return command;
+        }
+        
     }
 }
